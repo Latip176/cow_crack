cc0833b66b3d106761e6b8c3c1fdfdd1f4112e8d
Muhammad Latif Harkat <68332606+Latip176@users.noreply.github.com>
2022-03-02 09:53:44 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
+import optparse
+
+par = optparse.OptionParser(
+    usage = 'usage: %prog --bit 32 or --bit 64'
+)
+
+par.add_option(
+    '--bit',help='Masukan bit hp mu contoh: --bit 64',default=None
+)
+(arg,opt) = par.parse_args()
+
 if __name__ == "__main__":
    try:
-       __import__("app").hencet_memek()
+       if(arg.bit=="64"):
+       	__import__("app64").hencet_memek()
+       elif(arg.bit=="32"):
+       	__import__("app32").hencet_memek()
+       else:
+       	print(" * Jika hpmu 64 bit ketik: python run.py --bit 64, Jika hpmu 32 biy ketik: python run.py --bit 32\n")
    except Exception as e:
        exit(str(e))
-
-#contibuttors Yayan XD and Latip176
